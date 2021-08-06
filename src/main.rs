b0c7755921d544d9cdcb4c83140e42c2c26f185c
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-08-06 15:04:26 -0400

    Add some basic boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::serde::json::Json;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct User {
+    username: String,
+}
+
+#[post("/login", format = "application/json", data = "<user>")]
+async fn login(user: Json<User>) {
+    println!("{}", user.username);
+}
+
+#[launch]
+fn launch() -> _ {
+
+    rocket::build().mount("/", routes![login])
 }
